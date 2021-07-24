85b66f78ca623004aca28c2a48b262bcfc4cf2af
alondgr <alon.sela87@gmail.com>
2021-07-24 17:25:49 +0300

    first commit

---
+++ avatar-project/src/Card/Avatar.js
@@ -0,0 +1,5 @@
+
+export default function Avatar(props) {
+
+    return <img src={props.pokemon} alt={props.alt}  />
+}
---
+++ avatar-project/src/Card/Card.js
@@ -0,0 +1,8 @@
+import Avatar from "./Avatar";
+
+export default function Card() {
+    return <div>
+        <Avatar pokemon="https://i.pinimg.com/564x/dc/ab/b7/dcabb7fbb2f763d680d20a3d228cc6f9.jpg" alt="picachu" />
+        {/* <Avatar pokemon="https://i.pinimg.com/originals/af/7c/19/af7c1907e3d2e5f142b81ad6a667fd5c.jpg" alt="bal" /> */}
+    </div>;
+}
